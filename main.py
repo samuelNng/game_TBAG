a9274b277e75e26f6567119c5ce3dc56066063fd
samuelNng <nsamuelmnng@gmail.com>
2025-01-17 12:24:16 +0000

    link rooms

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
 from room import Room
 
-kitchen = Room("kitchen")
-ballroom = Room("ballroom")
-dining_hall = Room("dining_hall")
+kitchen = Room()
+kitchen.set_name("kitchen")
+kitchen.set_description("A dank and dirty room buzzing with flies")
+
+ballroom = Room()
+ballroom.set_name("ballrom")
+ballroom.set_description("A vast room with a shiny wooden floor")
+
+dining_hall = Room()
+dining_hall.set_name("dining_hall")
+dining_hall.set_description("A large room with ornate golden decorations")
 
 #print(kitchen.name)
-kitchen.set_description("A dank and dirty room buzzing with flies")
-print(kitchen.get_description())
+
+#print(kitchen.get_description())
+kitchen.RoomName()
+kitchen.describe()
+
+
+#print(ballroom.get_description())
+ballroom.RoomName()
+ballroom.describe()
+
+kitchen.link_room(dining_hall, "south")
+dining_hall.link_room(kitchen, "north")
+dining_hall.link_room(ballroom,"west")
+ballroom.link_room(dining_hall,"east")
+
+
+
+kitchen.get_details()
+dining_hall.get_details()
--- room.py
+++ room.py
@@ -1,11 +1,35 @@
 class Room:
-    def __init__(self,room_name):
-        self.name =room_name
+    def __init__(self):
+        self.name =None
         self.description= None
-    #getter
+        self.linked_rooms={}
+    #getter description
     def get_description (self):
         return self.description
-    #setter
+    #setter description
     def set_description(self, room_description):
         self.description = room_description
+
+    def describe(self):
+        print(self.description)
+
+    #getter name
+    def get_name (self):
+        return self.name
+    #setter name
+    def set_name(self, room_name):
+        self.name = room_name
+
+    def RoomName(self):
+        print(self.name)
     
+    def link_room(self, room_to_link, direction):
+        self.linked_rooms[direction] = room_to_link
+
+    def get_details(self):
+        print(self.name)
+        print("-------------------------")
+        print(self.description)
+        for direction in self.linked_rooms:
+            room = self.linked_rooms[direction]
+        print("The " + room.get_name()+ " is "+direction)
